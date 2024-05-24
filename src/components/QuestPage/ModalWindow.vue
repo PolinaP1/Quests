4c82be40c7b0b564534f924a0aadfd77922291c8
Polina <polinapuchkova357@gmail.com>
2024-05-24 03:52:19 +0300

    ModalWindow

---
+++ src/components/QuestPage/ModalWindow.vue
@@ -0,0 +1,89 @@
+<script>
+
+export default {
+    props: {
+        initialValue: {
+            type: Object,
+            required: true
+        },
+        reservation: {
+            type: Object,
+            required: true
+        },
+        todo: {
+            type: Object,
+            required: true
+        }
+    },
+    data() {
+        return {
+            hide: true
+        }
+    },
+    methods: {
+        hideModel() {
+            this.$emit('hideModel')
+        }
+
+        // IMask(
+        //     document.getElementById('phone-mask')
+        // {
+        //     mask: '+{7}(000)000-00-00'
+        // }
+        // )
+    }
+}
+</script>
+
+<template>
+    <!-- @click="hideModal" -->
+    <div class="modal" @click="hideModel" v-show="this.hide">
+        <div class="modal__content" @click="(e) => e.stopPropagation()">
+            <div class="modal__content__quest">
+                <img :src="require(`@/img/${todo.img}.svg`)" class="modal__content__quest__img" />
+                <div class="modal__content__quest__info">
+                    <h2 class="modal__content__quest__info__name">{{ todo.name }}</h2>
+                    <h2 class="modal__content__quest__info__address">Большая Октябрьская ул., 52А, Ярославль</h2>
+                    <div class="modal__content__quest__info__datetime">
+                        <h3>{{ initialValue.date }}, </h3>
+                        <h3>{{ initialValue.time }}</h3>
+                    </div>
+                </div>
+            </div>
+            <h1 class="modal__content__quest__title">Бронирование</h1>
+            <div class="modal__content__quest__inputs">
+                <div class="modal__content__quest__inputs__name">
+                    <h2 class="modal__content__quest__inputs__name__title">Имя:</h2>
+                    <input class="modal__content__quest__inputs__name__input" type="text" placeholder="Имя" />
+                </div>
+                <div class="modal__content__quest__inputs__number">
+                    <h2 class="modal__content__quest__inputs__number__title">Номер телефона:</h2>
+                    <input class="modal__content__quest__inputs__number__input" type="text"
+                        placeholder="+7(000)910-86-85" />
+                </div>
+            </div>
+            <div class="modal__content__quest__players">
+                <h2>Количество игроков:</h2>
+                <div class="modal__content__quest__players__counting">
+                    <button class="modal__content__quest__players__button"><img src="@/img/MinusNumber.svg" /></button>
+                    <h2>2</h2>
+                    <button class="modal__content__quest__players__button"><img src="@/img/PlusNumber.svg" /></button>
+                </div>
+            </div>
+            <div class="modal__content__quest__sum">
+                <h2>Общая сумма:</h2>
+                <div class="modal__content__quest__sum__price">
+                    <h3 v-for="price in reservation.price_reservation" :key="price" :price="price">{{ price.price }}
+                    </h3>
+                    <img src="@/img/RubleSum.svg" />
+                </div>
+            </div>
+            <div class="modal__content__quest__buttons">
+                <button class="modal__content__quest__buttons__cancel">Отменить</button>
+                <button class="modal__content__quest__buttons__reserve">Бронировать</button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style></style>
--- src/components/QuestPage/ScheduleQuestSelect.vue
+++ src/components/QuestPage/ScheduleQuestSelect.vue
@@ -1,5 +1,6 @@
 <script>
 import SheduleButton from '../UI/SheduleButton.vue';
+import ModalWindow from './ModalWindow.vue';
 import axios from 'axios';
 
 const baseReservationUrl = "http://localhost:5500/reservations";
@@ -14,12 +15,14 @@ export default {
 
     data() {
         return {
+            visibilityModal: false,
             reservations: [],
             active: "",
             visibleDate: false,
-            initialValue:{
-                type:'',
-                date:''
+            initialValue: {
+                type: '',
+                date: '',
+                time: ''
             }
         }
     },
@@ -34,7 +37,8 @@ export default {
         }
     },
     components: {
-        SheduleButton
+        SheduleButton,
+        ModalWindow
     },
 
     methods: {
@@ -42,9 +46,10 @@ export default {
             this.active = text;
             this.initialValue = {
                 type: text,
-                date:''
+                date: '',
+                time: ''
             }
-            
+
             if (text == "Выбрать период") {
                 this.visibleDate = true;
             }
@@ -53,16 +58,24 @@ export default {
             }
             console.log(this.initialValue)
         },
-        changeDate(date){
+        changeDate(date) {
             this.initialValue.date = date;
-            
         },
-        cancelDate(){
+        changeTime(time) {
+            this.initialValue.time = time;
+            this.showModal();
+        },
+        cancelDate() {
             this.visibleDate = false;
+        },
+        showModal() {
+            this.visibilityModal = true;
+        },
+        hideModel(){
+            this.visibilityModal = false;
         }
     },
-
-    computed: {
+    computed:{
         filteredReviews() {
             return this.reservations.filter(reservation => reservation.id_quest === this.todo.id);
         }
@@ -84,23 +97,31 @@ export default {
                 <div class="shedule__item__date__period">
                     <shedule-button
                         :class="[this.active == 'Выбрать период' ? 'shedule__item__date__active' : 'shedule__item__date__button']"
-                        @click="activeButton('Выбрать период')"> {{(this.initialValue.type == 'Выбрать период' && this.initialValue.date != '')? this.initialValue.date : 'Выбрать период'}}</shedule-button>
+                        @click="activeButton('Выбрать период')"> {{ (this.initialValue.type == 'Выбрать период' &&
+                        this.initialValue.date != '') ? this.initialValue.date : 'Выбрать период' }}</shedule-button>
                     <div class="shedule__item__date__period__option" v-show="visibleDate">
-                        <div class="shedule__item__date__period__option__reservation" v-for="reservation in filteredReviews" :key="reservation" :reservation="reservation">
-                            <div  class="shedule__item__date__period__option__reservation__hover" v-for="date in reservation.date_reservation"
-                                :key="date" :date="date">
-                                <h2 class="shedule__item__date__period__option__reservation__title" @click="changeDate(date.date)">{{ date.date }}</h2>
+                        <div class="shedule__item__date__period__option__reservation"
+                            v-for="reservation in filteredReviews" :key="reservation" :reservation="reservation">
+                            <div class="shedule__item__date__period__option__reservation__hover"
+                                v-for="date in reservation.date_reservation" :key="date" :date="date">
+                                <h2 class="shedule__item__date__period__option__reservation__title"
+                                    @click="changeDate(date.date)">{{ date.date }}</h2>
                             </div>
                         </div>
                     </div>
                 </div>
-                <button class="shedule__item__date__clear" v-show="visibleDate" @click="cancelDate"><img src="@/img/CancelFiltering.svg" /></button>
+                <button class="shedule__item__date__clear" v-show="visibleDate" @click="cancelDate"><img
+                        src="@/img/CancelFiltering.svg" /></button>
             </div>
             <div class="shedule__item__time">
                 <shedule-button :class="[time.isActive == false && 'shedule__item__time__button']"
-                    v-for="time in todo.time_reservation" :key="time" :time="time">{{ time.time }}</shedule-button>
+                    v-for="time in todo.time_reservation" :key="time" :time="time" @click="changeTime(time.time)">{{ time.time }}
+                </shedule-button>
             </div>
         </div>
+    </div>
+    <div v-show="visibilityModal" >
+        <modal-window @hideModel="hideModel"  v-for="reservation in filteredReviews" :key="reservation" :reservation="reservation" :initialValue="initialValue"  :todo="todo"/>
     </div>
 </template>
 
